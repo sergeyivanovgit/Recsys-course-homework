65fc401001120244aa9df87e1a783bf1c0f58c93
ekaterina_sheina <sheina.ekaterina.s@yandex.ru>
2021-09-24 17:13:39 +0300

    Stylistic fix

--- task_2_multiarmed_bandit/sim_lib.py
+++ task_2_multiarmed_bandit/sim_lib.py
@@ -1,6 +1,9 @@
-import pandas as pd
+
 import numpy as np
-from scipy.stats import beta, uniform, bernoulli, randint, expon
+import pandas as pd
+
+from scipy.stats import beta, uniform, bernoulli, expon
+from typing import Callable
 
 ALPHA = 1
 BETA = 20
@@ -10,11 +13,11 @@ MONITORING_FREQ = 10 ** 4
 
 def generate_new_banner(n, a=ALPHA, b=BETA, mu=MU):
     p = beta.rvs(a, b, size=n)
-    lt = expon.rvs(scale=mu, size=n)
-    return p, lt
+    lifetimes = expon.rvs(scale=mu, size=n)
+    return p, lifetimes
 
 
-def simulation(policy, n=10 ** 6, initial_banners=10):
+def simulation(policy: Callable, n=10 ** 6, initial_banners=10):
     state = pd.DataFrame(np.zeros((initial_banners, 4)), columns=['impressions', 'clicks', 'lifetime', 'p'])
     state['p'], state['lifetime'] = generate_new_banner(initial_banners)
     regret = 0
@@ -23,26 +26,25 @@ def simulation(policy, n=10 ** 6, initial_banners=10):
 
     for i in range(n):
         if uniform.rvs() < borning_rate or state.shape[0] < 2:
-            p, lt = generate_new_banner(1)
-            new_banner = pd.DataFrame({'impressions': 0, 'clicks': 0, 'lifetime': lt, 'p': p}, index=[max_index])
+            p, lifetime = generate_new_banner(1)
+            new_banner = pd.DataFrame({'impressions': 0, 'clicks': 0, 'lifetime': lifetime, 'p': p}, index=[max_index])
             state = pd.concat([state, new_banner], verify_integrity=True)
             max_index += 1
 
         index = policy(state[['impressions', 'clicks']].copy())
 
-        # assert isinstance(index, int), 'Error, wrong action type'
         assert index in state.index, 'Error, wrong action number'
 
         p = state.loc[index, 'p']
         reward = bernoulli.rvs(p)
         state.loc[index, 'impressions'] += 1
         state.loc[index, 'clicks'] += reward
-        regret = regret + max(state.p) - p
+        regret = regret + max(state['p']) - p
 
         state['lifetime'] = state['lifetime'] - 1
-        state = state.query(""" lifetime>0 """)
+        state = state[state['lifetime'] > 0]
 
-        if i % MONITORING_FREQ == 0:
+        if not i % MONITORING_FREQ:
             print('{} impressions have been simulated'.format(i + 1))
 
     return {'regret': regret, 'rounds': n, 'total_banners': max_index, 'history': state}
